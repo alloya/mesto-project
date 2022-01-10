76f26fc5fa328d8b6d51f52fe3a7a957737e0de3
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2022-01-10 21:41:12 +0300

    toggle popup, draw cards from aeeay

---
+++ script.js
@@ -0,0 +1,36 @@
+let cardsArray = [{'name': 'Карачаевск', 'image': './images/kirill-pershin-1088404-unsplash.jpg'},  {'name': 'Гора Эльбрус', 'image': './images/kirill-pershin-1404681-unsplash.png'}, {'name': 'Домбай', 'image': './images/kirill-pershin-1556355-unsplash.png'}, {'name': 'Гора Эльбрус', 'image': './images/kirill-pershin-1404681-unsplash.png'}, {'name': 'Карачаевск', 'image': './images/kirill-pershin-1088404-unsplash.jpg'}, {'name': 'Домбай', 'image': './images/kirill-pershin-1556355-unsplash.png'}];
+
+let cardsContainer = document.querySelector(".cards");
+let cardName = document.querySelector(".card__title");
+let cardImage = document.querySelector(".card__picture");
+let popUp = document.querySelector(".popup__overlay");
+let profileOpenButton = document.querySelector(".profile__edit-button");
+let profileCloseButton = document.querySelector(".popup__close");
+let profileSaveButton = document.querySelector(".popup__button-save");
+
+function renderCards(cards) {
+  if (cards.length !== 0) {
+    cards .forEach(element => {
+      cardsContainer.insertAdjacentHTML('beforeend', 
+        `<div class="card">
+          <img src=${element.image} alt=${element.name} class="card__picture">
+          <div class="card__title-wrapper">
+            <h2 class="card__title">${element.name}
+            </h2>
+            <button class="card__heart" type="button"></button>
+          </div>
+        </div>`
+      );}
+    )
+  }
+
+}
+
+function togglePopup() {
+  popUp.classList.toggle("popup_opened");
+}
+
+renderCards(cardsArray);
+profileOpenButton.addEventListener('click', togglePopup);
+profileCloseButton.addEventListener('click', togglePopup);
+profileSaveButton.addEventListener('click', togglePopup);
