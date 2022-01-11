1a81d321fe0139d9311889a6b903b1f90304fb44
Yulia Lushinskaya <ylu@listglobally.com>
2022-01-11 16:55:28 +0300

    toggle like

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let cardsArray = [ {'name': 'test', 'image': 'https://new-widget.kiwitaxi.com/static/images/Economy.png', 'like': true }, { 'name': 'Карачаевск', 'image': './images/kirill-pershin-1088404-unsplash.jpg', 'like': false }, { 'name': 'Гора Эльбрус', 'image': './images/kirill-pershin-1404681-unsplash.png', 'like': false }, { 'name': 'Домбай', 'image': './images/kirill-pershin-1556355-unsplash.png', 'like': false }, { 'name': 'Гора Эльбрус', 'image': './images/kirill-pershin-1404681-unsplash.png', 'like': false }, { 'name': 'Карачаевск', 'image': './images/kirill-pershin-1088404-unsplash.jpg', 'like': true }, { 'name': 'Домбай', 'image': './images/kirill-pershin-1556355-unsplash.png', 'like': false }];
 
 let cardsContainer = document.querySelector(".cards");
+let cards;
 let profilePopup = document.querySelector(".popup__overlay");
 let profileOpenButton = document.querySelector(".profile__edit-button");
 let profileCloseButton = document.querySelector(".popup__close");
@@ -11,6 +12,8 @@ let cardSaveButton = document.querySelector(".cards-popup .popup__button-save");
 let cardsCloseButton = document.querySelector(".cards-popup .popup__close");
 let cardTrashBins = document.querySelectorAll(".card__trash");
 let cardLikes = document.querySelectorAll(".card__heart");
+let fullImageContainer = document.querySelector(".image-popup");
+let fullImageContainerCloseButton = fullImageContainer.querySelector(".popup__close");
 
 function initializeCardsList() {
   if (cardsArray.length) {
@@ -18,24 +21,34 @@ function initializeCardsList() {
       renderCards(element);
     })
   }
+  debugger
+  cards = cardsContainer.querySelectorAll(".card");
 }
 
 function renderCards(card, type = 'beforeend') {
-  let likeClass;
+  let likeClass = '';
   if (card.like) {
     likeClass = 'card__heart_inverted';
   }
   cardsContainer.insertAdjacentHTML(type,
     `<div class="card">
-      <img src=${card.image} alt=${card.name} class="card__picture">
+      <img src=${card.image} alt=${card.name} class="card__picture" onclick="showFullImage(this)">
       <button class="card__trash" type="button"></button>
       <div class="card__title-wrapper">
         <h2 class="card__title">${card.name}
         </h2>
-        <button class="card__heart ${likeClass}" type="button"></button>
+        <button class="card__heart ${likeClass}" type="button" onclick="toggleLike(this)"></button>
       </div>
     </div>`
   )
+}
+
+function showFullImage(card) {
+  fullImageContainer.insertAdjacentHTML('beforeend', 
+  `<img src="${card.image}" class="card__full-picture">
+    <p class="card__full-subtitle">${card.name}</p>`
+  );
+  toggleFullImagePopup();
 }
 
 function submitProfileForm(evt) {
@@ -72,10 +85,28 @@ function toggleCardPopup() {
   cardPopup.classList.toggle("popup_opened");
 }
 
+function toggleFullImagePopup() {
+  fullImageContainer.classList.toggle("popup_opened");
+}
+
+function toggleLike(target) {
+  target.classList.toggle("card__heart_inverted");
+}
+
+// function initCards() {
+//   if (cards.length) {
+//     cards.forEach(element => {
+//       element.addEventListener('click', showFullImage);
+//     });
+//   }
+// }
+
 initializeCardsList();
+// initCards();
 profileOpenButton.addEventListener('click', toggleProfilePopup);
 profileCloseButton.addEventListener('click', toggleProfilePopup);
 profileSaveButton.addEventListener('click', submitProfileForm);
 cardAddButton.addEventListener('click', toggleCardPopup);
 cardSaveButton.addEventListener('click', submitCardForm);
 cardsCloseButton.addEventListener('click', toggleCardPopup);
+fullImageContainerCloseButton.addEventListener('click', toggleFullImagePopup);
